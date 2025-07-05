21c184b61f4bdbd4ef0edc406e864968a683362f
Adrian Leroy <127160585+DragonesMagicos@users.noreply.github.com>
2025-07-05 14:09:33 -0300

    Add files via upload

--- src/main/java/com/techlab/ferreteria_api/controller/ClienteController.java
+++ src/main/java/com/techlab/ferreteria_api/controller/ClienteController.java
@@ -3,6 +3,7 @@ package com.techlab.ferreteria_api.controller;
 import com.techlab.ferreteria_api.model.Cliente;
 import com.techlab.ferreteria_api.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,5 +17,17 @@ public class ClienteController {
     public Cliente loginOrRegister(@RequestBody Cliente cliente) {
         return clienteService.loginOrRegister(cliente);
     }
+    @GetMapping("/email/{email}")
+    public ResponseEntity<Cliente> getClienteByEmail(@PathVariable String email) {
+        Cliente cliente = clienteService.findByEmail(email);
+        if (cliente != null) {
+            // Si se encuentra, devuelve el cliente con un estado 200 OK.
+            return ResponseEntity.ok(cliente);
+        } else {
+            // Si no se encuentra, devuelve un estado 404 Not Found.
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
 
